cb13919390c5287f9a005c3d8e5f476ed25ffe05
Robert Feldt <robert.feldt@gmail.com>
2020-05-02 12:31:48 +0200

    a few more example runs

--- examples/bankruptcy_dataset.jl
+++ examples/bankruptcy_dataset.jl
@@ -6,5 +6,7 @@ using InterpretableModels
 df = CSV.read(joinpath(@__DIR__(), "data", "bankruptcy_processed.csv"))
 
 classcolumn = 1
-bslim1 = bboptimize_slim_scoring_system(df, classcolumn, 0.1; MaxTime = 3.0, PopSize = 100)
-bslim2 = bboptimize_slim_scoring_system(df, classcolumn, 0.01; MaxTime = 10.0, PopSize = 200)
+
+bslim1   = bboptimize_slim_scoring_system(df, classcolumn, 1; MaxTime = 5.0, PopSize = 200)
+bslim01  = bboptimize_slim_scoring_system(df, classcolumn, 0.1; MaxTime = 5.0, PopSize = 200)
+bslim001 = bboptimize_slim_scoring_system(df, classcolumn, 0.01; MaxTime = 5.0, PopSize = 200)
